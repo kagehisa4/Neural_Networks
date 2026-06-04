43b27a58ca7d62a95833eb6948cf77b85e0c29f5
kagehisa4 <kagehisa@proton.me>
2026-06-04 10:33:57 +0530

    modified for better readability

--- perceptrons/iris/test.py
+++ perceptrons/iris/test.py
@@ -6,14 +6,14 @@ import reader
 with open("ppn.pkl", "rb") as f:
     ppn = pickle.load(f)
 
-# test
-'''sl = float(input('sepal_l'))
-pl = float(input('petal_l'))
+# test from user input
+sl = float(input('sepal_length'))
+pl = float(input('petal_length'))
 
 
 output = ppn.predict([sl,pl])
 
-print('Iris-serosa') if output == 1 else print('Iris-versicolor')'''
+print('Iris-serosa') if output == 1 else print('Iris-versicolor')
 
 #print errors from the list:
 plot_decision_regions(reader.X, reader.y, classifier = ppn)
