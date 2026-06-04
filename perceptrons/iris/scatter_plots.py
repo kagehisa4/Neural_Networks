5b0f8e6925fd33a394328d6783775c59498c6de5
kagehisa4 <kagehisa@proton.me>
2026-06-04 10:15:05 +0530

    scatter_plots FOR ALL CLASSES

--- perceptrons/iris/scatter_plots.py
+++ perceptrons/iris/scatter_plots.py
@@ -3,25 +3,25 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('iris.csv')
 
-for i in range(50,100):
-    if i ==50:
-        plt.scatter(df['sl'].iloc[i], df['pw'].iloc[i],
+for i in range(50):
+    if i ==0:
+        plt.scatter(df['sw'].iloc[i], df['pl'].iloc[i],
         marker = 'o', color = 'green', alpha = .8,
-        label = 'iris-versicolor')
-    plt.scatter(df['sl'].iloc[i], df['pw'].iloc[i],
+        label = 'iris-setosa')
+    plt.scatter(df['sw'].iloc[i], df['pl'].iloc[i],
         marker = 'o', color = 'green', alpha = .8,
         )
-for i in range(100, 150):
-    if i ==100:
-        plt.scatter(df['sl'].iloc[i], df['pw'].iloc[i],
+for i in range(50,100):
+    if i == 50:
+        plt.scatter(df['sw'].iloc[i], df['pl'].iloc[i],
         marker = 'x', color = 'red', alpha = .8,
-        label = 'iris-verginica')
+        label = 'iris-versicolor')
     else:
-        plt.scatter(df['sl'].iloc[i], df['pw'].iloc[i],
+        plt.scatter(df['sw'].iloc[i], df['pl'].iloc[i],
         marker = 'x', color = 'red', alpha = .8,
         )
 
-plt.xlabel('sepal length')
+plt.xlabel('petal length')
 plt.ylabel('petal width')
 plt.legend(loc = 'best')
 plt.show()
