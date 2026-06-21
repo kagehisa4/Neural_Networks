225837bbc4f37758b799634f86c8224170bcde2d
kagehisa4 <kagehisa@proton.me>
2026-06-21 22:31:41 +0530

    update

--- perceptrons/MNIST/digits_count.py
+++ perceptrons/MNIST/digits_count.py
@@ -1,5 +1,4 @@
 from sklearn.datasets import load_digits
-import sklearn
 
 digits = load_digits()
 
@@ -8,12 +7,9 @@ y = digits.target
 
 hm = {}
 
-digits = {0,1,2,3,4,5,6,7,8,9}
-
 for i in range (len(y)):
     if y[i] not in hm:
         hm[int(y[i])] = 0
-    if y[i] in digits:
-        hm[y[i]] += 1
+    hm[y[i]] += 1
 
 print(hm)
